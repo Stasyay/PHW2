26699ab433dcbed13038d1882d79482a6b8fd5b7
Анастасия Тибейкина <Nastya@kompas.lan>
2022-11-30 10:02:54 +0600

    add task5

---
+++ task5.py
@@ -0,0 +1,11 @@
+# Реализуйте алгоритм перемешивания списка.
+import random
+
+lst = ["klj", "8jkh7", 1, "khf", "98sd,f"]
+
+for i in range(len(lst)):
+    index_new = random.randrange(0,len(lst))
+    temp = lst[i]
+    lst[i] = lst[index_new]
+    lst[index_new] = temp
+print(lst)
