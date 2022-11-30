d90c8c312596a33a7bf20edb52e656002baca0ac
Анастасия Тибейкина <Nastya@kompas.lan>
2022-11-30 10:04:43 +0600

    task5 самое простое решение

--- task5.py
+++ task5.py
@@ -3,9 +3,12 @@ import random
 
 lst = ["klj", "8jkh7", 1, "khf", "98sd,f"]
 
-for i in range(len(lst)):
-    index_new = random.randrange(0,len(lst))
-    temp = lst[i]
-    lst[i] = lst[index_new]
-    lst[index_new] = temp
+random.shuffle(lst)
 print(lst)
+
+# for i in range(len(lst)):
+#     index_new = random.randrange(0,len(lst))
+#     temp = lst[i]
+#     lst[i] = lst[index_new]
+#     lst[index_new] = temp
+# print(lst)
